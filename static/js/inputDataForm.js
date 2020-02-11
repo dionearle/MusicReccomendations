bc0df15aebc9a1243d85b8a89419a6b5e6f72bc8
Dion Earle <dion.earle@gmail.com>
2020-02-11 15:03:58 +1100

    Releases are now displayed on the webpage after the search is complete, so the user can see the recommendations. Also attempted to filter out all albums a user has heard before, but this significantly slowed down the process.

--- static/js/inputDataForm.js
+++ static/js/inputDataForm.js
@@ -33,6 +33,14 @@ function handleInputForm() {
         const searchSettingsHTML = document.getElementById('searchSettings');
         searchSettingsHTML.parentNode.removeChild(searchSettingsHTML);
         
+        // here we are setting up the progress bar for the following search
+        const outerProgress = document.getElementById('outer-progress');
+        outerProgress.style.visibility = 'visible';
+        const progressBar = document.getElementById('progress');
+        
+        const status = document.getElementById('status');
+        status.textContent = 'Getting your last.fm friends...';
+
         // here we specify the url for the fetch request
         let url = new URL('http://ws.audioscrobbler.com/2.0/');
 
@@ -66,7 +74,7 @@ function handleInputForm() {
                 // pages of friends this user has.
                 // now we have this, we can retrieve the friends page by page
                 let totalPages = response.friends['@attr'].totalPages;
-                
+
                 // TODO: Rather than making another fetch request after getting the total
                 // pages, should probably get friends info for the first page and then
                 // if necessary loop through all remaining pages (e.g. starting from i = 2)
@@ -87,6 +95,9 @@ function handleInputForm() {
                     friendsPromises.push(fetch(url, options)
                         .then(response => response.json())
                     );
+
+                    let progress = (i / totalPages) * 100;
+                    progressBar.style.width = progress + '%';
                 }
 
                 Promise.all(friendsPromises)
@@ -104,14 +115,17 @@ function handleInputForm() {
                             }
                         }
 
-                        const topAlbumsPromises = []
+                        const topAlbumsPromises = [];
 
                         // here we go through each friend and get their top albums,
                         // artists and tracks. we also use the time period as selected
                         // in the search parameters earlier
+                        const status = document.getElementById('status');
+                        status.textContent = 'Getting your friend\'s top albums...';
 
                         // TODO: implement a loading bar like I did on newReleaseTracker
                         for (let i = 0; i < friends.length; i++) {
+                            
                             // TODO: implement this for top artists and tracks as well
                             
                             let query = {
@@ -128,6 +142,9 @@ function handleInputForm() {
                             topAlbumsPromises.push(fetch(url, options)
                                 .then(response => response.json())
                             );
+
+                            let progress = (i / friends.length) * 100;
+                            progressBar.style.width = progress + '%';
                         }
 
                         Promise.all(topAlbumsPromises)
@@ -136,25 +153,26 @@ function handleInputForm() {
                             let results = [];
 
                             for (let i = 0; i < responses.length; i++) {
-
+                                
                                 const response = responses[i];
 
                                 for (let i = 0; i < response.topalbums.album.length; i++) {
 
                                     let album = response.topalbums.album[i];
 
-                                    // TODO: can also extract album art from this, which
-                                    // will be helpful when displaying results to user
-
                                     let score = 51 - album['@attr'].rank;
                                     let name = album.name;
                                     let artist = album.artist.name;
                                     let playcount = parseInt(album.playcount);
-
+                                    let mbid = album.mbid;
+                                    let image = album.image[3]['#text'];
+                                    // TODO: also store release date to display
+                                    
                                     let found = false;
 
                                     for (let i = 0; i < results.length; i++) {
-                                        if (results[i].name === name) {
+                                        if (results[i].name === name
+                                        && results[i].artist === artist) {
                                             results[i].score += score;
                                             results[i].playcount += playcount;
                                             found = true;
@@ -168,7 +186,9 @@ function handleInputForm() {
                                             'score': score,
                                             'name': name,
                                             'artist': artist,
-                                            'playcount': playcount
+                                            'playcount': playcount,
+                                            'mbid': mbid,
+                                            'image': image
                                         });
                                     }        
                                 }
@@ -179,12 +199,115 @@ function handleInputForm() {
                             // your friends
                             results.sort((a, b) => parseFloat(b.score) - parseFloat(a.score));
                             //results.sort((a, b) => parseFloat(b.playcount) - parseFloat(a.playcount));
+
                             
-                            console.log(results);
+                            // if we aren't showing all releases, then we need to check through each one to
+                            // see if the last.fm user has this release in their library
+                            if (!searchType) {
+
+                                const status = document.getElementById('status');
+                                status.textContent = 'Filtering out releases you have already heard...';
+                                
+                                // use the album.getInfo request, giving the mbid and last fm username.
+                                // the response will give the user's playcount for this release, and if 0 (or null)
+                                // then we keep the release. otherwise we can remove
+
+                                // TODO: COULD ALSO HAVE OPTION WHERE IF PLAYCOUNT IS LESS THAN GIVEN AMOUNT
+                                // IT IS STILL DISPLAYED
+                                // e.g. 'Display all releases with less than [] plays (default is to display all)' when searching
+
+                                const listenedBeforePromises = [];
+                                
+                                // TODO: find a way to figure out when we have 50 unheard releases,
+                                // as this takes really long and still eventually shows albums you've heard
+                                for (let i = 0; i < 100; i++) {
+
+                                    let query = {
+                                        method: 'album.getInfo',
+                                        artist: results[i].artist,
+                                        album: results[i].name,
+                                        username: username,
+                                        api_key: '3e0c61f86ab0621665f8bb0bccd2eaf9',
+                                        format: 'json'
+                                    };
+        
+                                    // we then add this search query to the url
+                                    url.search = new URLSearchParams(query);
+        
+                                    listenedBeforePromises.push(fetch(url, options)
+                                        .then(response => response.json())
+                                    );
+        
+                                    let progress = (i / results.length) * 100;
+                                    progressBar.style.width = progress + '%';
+                                }
+
+                                Promise.all(listenedBeforePromises)
+                                .then(responses => {
+
+                                    for (let i = 0; i < responses.length; i++) {
+
+                                        const response = responses[i];
+
+                                        // if the last.fm user's playcount for this release isn't 0,
+                                        // then we don't want to display it to them
+                                        if (response.album.userplaycount !== '0') {
+                                            
+                                            // TODO: not the best solution, as am altering
+                                            // the score to 0, and then resorting the list once
+                                            // going through it all
+                                            results[i].score = 0;
+                                        }
+
+                                    }
+
+                                    results.sort((a, b) => parseFloat(b.score) - parseFloat(a.score));
+
+                                    outerProgress.style.visibility = 'hidden';
+                                    status.textContent = '';
+
+                                    displayHelper(results);
+                                });
+                            } else {
+
+                                outerProgress.style.visibility = 'hidden';
+                                status.textContent = '';
+
+                                displayHelper(results);
+                            }
                         });
 
 
                     });
             });
         }    
+}
+
+function displayHelper(results) {
+    // TODO: for now we will leave it at 50, hopefully set up some form
+    // of pagination so they can view more than 50 results
+    for (let i = 0; i < 50; i++) {
+        displayRelease(results[i]);
+    }
+}
+
+function displayRelease(album) {
+
+    const html = document.getElementById('display-releases');
+
+    const release = document.createElement('div');
+    release.classList.add('col-3');
+    release.style.align = 'center';
+
+    const releaseText = document.createElement('p');
+    releaseText.textContent = album.artist + ': ' + album.name + ' (score: ' + album.score + ' & playcount: ' + album.playcount + ')';
+    release.appendChild(releaseText);
+
+    const coverArt = document.createElement('img');
+    coverArt.src = album.image;
+    coverArt.style.height = '100px';
+    coverArt.style.width = '100px';
+    release.insertBefore(coverArt, releaseText);
+
+    html.appendChild(release);
 }
